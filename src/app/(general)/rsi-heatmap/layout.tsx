51b6a8175ce1a73b09bc80b48980d4ed2341d3c5
vuthinh0301 <vuthinh0301@gmail.com>
2024-06-04 13:16:13 +0700

    feat: add footer

--- src/app/(general)/rsi-heatmap/layout.tsx
+++ src/app/(general)/rsi-heatmap/layout.tsx
@@ -1,4 +1,6 @@
+import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/header";
+
 import Link from "next/link";
 
 export default function RsiHeatMapLayout({
@@ -7,18 +9,20 @@ export default function RsiHeatMapLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="mx-2 sm:mx-4 md:mx-10 lg:mx-16 xl:mx-20 mt-7 mb-40">
-      <Header />
-      <div className="flex items-center justify-center text-center text-xs leading-5 font-medium  bg-[#F0F7FF] h-11 rounded-lg mt-8">
-        <div>
-          Bitcoin price drops 10% in the last 24 hours!
-          <Link href="#" className="text-[#1A64F0] ml-1">
-            SEE MORE!
-          </Link>
+    <div>
+      <section className="mx-2 sm:mx-4 md:mx-10 lg:mx-16 xl:mx-20 mt-7 mb-40">
+        <Header />
+        <div className="flex items-center justify-center text-center text-xs leading-5 font-medium  bg-[#F0F7FF] h-11 rounded-lg mt-8">
+          <div>
+            Bitcoin price drops 10% in the last 24 hours!
+            <Link href="#" className="text-[#1A64F0] ml-1">
+              SEE MORE!
+            </Link>
+          </div>
         </div>
-      </div>
-
-      <div className="mt-6 md:mt-8">{children}</div>
-    </section>
+        <div className="mt-6 md:mt-8 mb-30">{children}</div>
+      </section>
+      <Footer />
+    </div>
   );
 }
